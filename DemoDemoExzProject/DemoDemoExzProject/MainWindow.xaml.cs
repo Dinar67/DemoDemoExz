388b13850096a665b61937e5a2f7f4e0fa6a9c85
212111 <212111@1305-013>
2025-02-28 13:56:38 +0300

    Added Navigation

--- DemoDemoExzProject/DemoDemoExzProject/MainWindow.xaml.cs
+++ DemoDemoExzProject/DemoDemoExzProject/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DemoDemoExzProject.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DemoDemoExzProject
 {
@@ -23,6 +11,8 @@ namespace DemoDemoExzProject
         public MainWindow()
         {
             InitializeComponent();
+            Navigation.Initiaize(this);
+            Navigation.NextPage(new MainPage());
         }
     }
 }
