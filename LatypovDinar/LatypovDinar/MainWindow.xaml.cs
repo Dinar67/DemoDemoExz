8cca40f4fc39ef39686faa92dd86f768f9cabbec
212111 <212111@1305-013>
2025-02-28 14:52:07 +0300

    Test

---
+++ LatypovDinar/LatypovDinar/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using LatypovDinar.Pages;
+using System.Windows;
+
+namespace LatypovDinar
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Navigation.Initialize(this);
+            Navigation.NextPage(new MainPage());
+        }
+    }
+}
---
+++ LatypovDinar/LatypovDinar/Navigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+
+namespace LatypovDinar
+{
+    public static class Navigation
+    {
+        public static MainWindow MainWindow { get; private set; }
+        public static void Initialize(MainWindow window)
+        {
+            MainWindow = window;
+        }
+
+        public static void NextPage(Page page)
+        {
+            if (MainWindow != null) MainWindow.MainFrame.Navigate(page);
+        }
+
+        public static void BackPage()
+        {
+            if (MainWindow != null && MainWindow.MainFrame.CanGoBack) MainWindow.MainFrame.GoBack();
+        }
+    }
+}
